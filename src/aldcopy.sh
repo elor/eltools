4e54d577e1a770b276a44fb4caab32683d6bf442
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-05-06 16:18:50 +0200

    aldcopy: valgrindsimulation added

--- src/aldcopy.sh
+++ src/aldcopy.sh
@@ -8,5 +8,5 @@ cd "$src"
 cp -r "`which parsivald`" input lmpout options.cfg pyprefs "$dst"
 
 mkdir -p "$dst/Scripts"
-cp -r Scripts/debugsimulation.sh Scripts/runsimulation.sh Scripts/startworkers.sh Scripts/screenlocal.sh Scripts/localjob.sh "$dst/Scripts/"
+cp -r Scripts/debugsimulation.sh Scripts/valgrindsimulation.sh Scripts/runsimulation.sh Scripts/startworkers.sh Scripts/screenlocal.sh Scripts/localjob.sh "$dst/Scripts/"
 
