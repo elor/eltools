6d89e3aca415ba2f9b68b056e2f7a7bd27c7d390
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-05-06 11:32:18 +0200

    aldcopy: lammps->input

--- src/aldcopy.sh
+++ src/aldcopy.sh
@@ -5,7 +5,7 @@ dst="`pwd`"
 
 cd "$src"
 
-cp -r "`which parsivald`" lammps lmpout options.cfg pyprefs "$dst"
+cp -r "`which parsivald`" input lmpout options.cfg pyprefs "$dst"
 
 mkdir "$dst/Scripts"
 cp -r Scripts/debugsimulation.sh Scripts/runsimulation.sh Scripts/startworkers.sh Scripts/screenlocal.sh Scripts/localjob.sh "$dst/Scripts/"
