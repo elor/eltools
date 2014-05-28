de4f40ccb2ddd753929b7e982f83b0a670f09da8
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-05-28 10:25:09 +0200

    executequicksynergy.sh: only start one instance

--- src/executequicksynergy.sh
+++ src/executequicksynergy.sh
@@ -2,18 +2,23 @@
 # 
 # starts quicksynergy and uses xdotool to 
 
+getwinid(){
+  xwininfo -name QuickSynergy | sed -n 's/^.*Window id:\s*\(0x[0-9a-fA-F]*\).*$/\1/p'
+}
+
 type xwininfo >/dev/null || exit 1
 type xdotool >/dev/null || exit 1
 type sed >/dev/null || exit 1
 type quicksynergy >/dev/null || exit 1
+
+[ -n "`getwinid`" ] && echo "quicksynergy is already running" >&2 && exit 1
 
 quicksynergy &>/dev/null &
 
 winid=''
 while [ -z "$winid" ]; do
   sleep 0.1
-  winid=`xwininfo -name QuickSynergy | sed -n 's/^.*Window id:\s*\(0x[0-9a-fA-F]*\).*$/\1/p'`
-  echo "winid: '$winid'"
+  winid=`getwinid`
 done
 
 xdotool key --window $winid alt+e
