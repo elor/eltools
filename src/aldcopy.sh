c11edf28fd70ce84db8459a890684da2d1727313
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-06-26 16:19:49 +0200

    changelogline added; aldcopy removed

--- src/aldcopy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sourcedir=/home/e.lorenz/code/parsivald
-destination="`pwd`"
-
-cd "$sourcedir"
-
-cp -r input lmpout options.cfg pyprefs "$destination"
-
---
+++ src/changelogline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# print changelog line for the current date and git user
+
+user=`git config user.name`
+[ "$user" ] || user="Undefined User"
+mail=`git config user.email`
+[ "$mail" ] || mail="user@mail.com"
+date=`date -Idate`
+
+echo $date$'\t'$user$'\t<'$mail'>'
+
