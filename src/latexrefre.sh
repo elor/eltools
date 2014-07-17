41b4e315f0359a44989f441545d1143154a6e31d
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-07-17 10:58:29 +0200

    latexrefre.sh: sending error notifications, processing modification time

--- src/latexrefre.sh
+++ src/latexrefre.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-src="$1"
+[ $1 ] && src="$1" || src="masterthesis.tex"
 if [ -z "$src" ]; then
-  echo "usage: $0 <input.tex>"
-  exit
+    echo "usage: $0 <input.tex>"
+    exit
 fi
 
 gethash(){
-  find . -name '*.tex' | xargs stat -c %y $f | md5sum
+	  find . -name '*.tex' | grep -v '#' | xargs stat -c %y | md5sum
+}
+
+senderror(){
+	  if type notify-send &>/dev/null; then
+		    notify-send -t 1 "latexrefre.sh: $@"
+	  else
+		    echo "couldn't find'notify-send'"
+	  fi
+
+	  echo "$@" >&2
 }
 
 oldhash=""
 
 while true; do
-  ls > /dev/null || exit 1
+    ls > /dev/null || exit 1
 
-  newhash=`gethash`
+    newhash=`gethash`
 
-  if [ "$oldhash" != "$newhash" ]; then
-    pdflatex --halt-on-error --interaction=nonstopmode "$src" 
-    oldhash="$newhash"
-  else
-    if read -t 0; then
-      read line
-      oldhash=""
+    if [ "$oldhash" != "$newhash" ]; then
+        pdflatex --halt-on-error --interaction=nonstopmode "$src" || senderror 'pdflatex failed'
+        oldhash="$newhash"
     else
-      sleep 0.1
+        if read -t 0; then
+            read line
+            oldhash=""
+        else
+            sleep 0.1
+        fi
     fi
-  fi
 
 done
 
