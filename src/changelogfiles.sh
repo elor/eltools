bee21e992ed2cca920f33dc7fcee477a210f4209
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2014-08-25 17:06:17 +0200

    changelogfiles.sh added

---
+++ src/changelogfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# print changelog line for the current date and git user
+
+listfiles(){
+    # list staged files
+    git diff --name-only --cached
+    # list changed files
+    git diff --name-only
+}
+
+listfiles | sort -hu | sed -e 's/^/\t* /' -e 's/$/: \n/'
+
