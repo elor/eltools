0617a84cb38cca18b30a3b24a286c892b0326526
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-01-21 16:22:38 +0100

    pullall.sh: prettier and safer directory traversal

--- src/pullall.sh
+++ src/pullall.sh
@@ -3,12 +3,14 @@
 # pull all
 
 for dir in */;do 
-	pushd $dir
-	branches=`git for-each-ref --shell --format='%(refname)' refs/heads/ | sed "s/'refs\/heads\/\(.*\)'/\1/"`
-	for branch in $branches; do
-		git checkout $branch >/dev/null
-		git pull --ff-only --all >/dev/null
-	done
-	popd
+    (
+        cd "$dir" && {
+            echo "=====     `basename "$dir"`     ====="
+	          branches=`git for-each-ref --shell --format='%(refname)' refs/heads/ | sed "s/'refs\/heads\/\(.*\)'/\1/"`
+	          for branch in $branches; do
+		            git checkout $branch >/dev/null
+		            git pull --ff-only --all >/dev/null
+	          done
+        }
+    )
 done
-
