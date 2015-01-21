179d2976d71d9774e0c3556d298e7e47f769f827
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-01-21 16:57:07 +0100

    pushall.sh: prettier texts

--- src/pushall.sh
+++ src/pushall.sh
@@ -6,30 +6,36 @@ error=false
 
 srcdir=`dirname $0`
 if [ -n "$1" ]; then
-  if [ -d "$1" ]; then
-    srcdir="$1"
-  else
-    echo "argument is no directory: '$1'" >&2
-    exit 1
-  fi
+    if [ -d "$1" ]; then
+        srcdir="$1"
+    else
+        echo "argument is no directory: '$1'" >&2
+        exit 1
+    fi
 fi
 
 cd "$srcdir"
 
-[ "`echo */.git`" != "*/.git" ] && for dir in */.git; do
-  cd "$dir/.." || continue
-  for remote in `git remote`; do
-    echo "$dir $remote"
-    git push --all $remote || error=true
-    git push $remote master --tags || error=true
-  done
-  cd -
-done
+if [ "`echo */.git`" != "*/.git" ]; then
+    for dir in */.git; do
+        (
+            echo "=====     $PWD/`dirname "$dir"`    ====="
+            cd "`dirname "$dir"`" && {
+                for remote in `git remote`; do
+                    echo "=====     `dirname "$dir"` ($remote)     ====="
+                    git push --all $remote || error=true
+                    git push $remote master --tags || error=true
+                done
+            }
+        )
+    done
+fi
 
-[ "`echo */pushall.sh`" != "*/pushall.sh" ] && for file in */pushall.sh; do
-  ./pushall.sh `dirname $file` || error=true
-done
+if [ "`echo */pushall.sh`" != "*/pushall.sh" ]; then
+    for file in */pushall.sh; do
+        ./pushall.sh `dirname $file` || error=true
+    done
+fi
 
 # exit
-! $error
-
+$error && exit 1
