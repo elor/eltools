942892f511be29f92bc90fed294920333e779c53
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-02-03 14:59:44 +0100

    kerbalpodcast-dl.sh: script for downloading kerbal podcasts added

---
+++ src/kerbalpodcast-dl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Downloads the latest kerbal podcast
+
+getlink(){
+    curl -s kerbalpodcast.libsyn.com | grep -Pom1 'https?://[A-Za-z._/]+[0-9]{3}\.mp3'
+}
+
+link=`getlink` || { echo "Download link extraction failed"; exit 1; }
+file=`basename $link`
+
+[ -z "$link" ] && { echo "Download link cannot be extracted from kerbalpodcast.libsyn.com.">&2; exit 1; }
+
+echo "download link: $link"
+echo "output file: $file"
+echo
+
+wget "$link"
