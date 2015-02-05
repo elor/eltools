d56c2b7a5d6b847b977aa910ed78fe6e0aa5ee7b
Erik E. Lorenz <erik.e.lorenz@gmail.com>
2015-02-05 14:37:46 +0100

    pullall.sh: text centering

--- src/git/pullall.sh
+++ src/git/pullall.sh
@@ -2,10 +2,18 @@
 #
 # pull all
 
+center(){
+    local str="$1"
+    local l=${#str}
+    local h=$((l/2))
+    local hwidth=10
+    printf '=====%*s%-*s =====\n' $hwidth ${str:0:h} $hwidth ${str:h}
+}
+
 for dir in */;do 
     (
         cd "$dir" && {
-            echo "=====     `basename "$dir"`     ====="
+            center `basename "$dir"`
 	          branches=`git for-each-ref --shell --format='%(refname)' refs/heads/ | sed "s/'refs\/heads\/\(.*\)'/\1/"`
 	          for branch in $branches; do
 		            git checkout $branch >/dev/null
